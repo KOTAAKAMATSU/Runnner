400731cb2a2e2d9764d19e5b23e7bb6fbe14b17e
KOTAAKAMATSU <redjuly2015@gmail.com>
2015-06-08 21:00:26 +0900

    坂道の生成試行中、キャラアクション実装中

--- Assets/FlowFloor.cs
+++ Assets/FlowFloor.cs
@@ -6,15 +6,13 @@ public class FlowFloor : MonoBehaviour {
 	public Vector3 speed    = Vector3.zero; //1フレームで動く距離(マイナスは逆方向)
 	public Vector3 distance = Vector3.zero; //この距離まで動く
 	
-	//distanceまで動いた後に反対方向へ折り返して動くか？
-	//falseだとdistanceまで動いたらそこで止る
-	public bool turn = true;
+
 	
 	private Vector3 moved = Vector3.zero; //移動した距離を保持
 
 	
 	void Update() {
-		//床を動かす
+		/*//床を動かす
 		float x = speed.x;
 		float y = speed.y;
 		float z = speed.z;
@@ -23,12 +21,11 @@ public class FlowFloor : MonoBehaviour {
 		//動いた距離を保存
 		moved.x += Mathf.Abs(speed.x);
 		moved.y += Mathf.Abs(speed.y);
-		moved.z += Mathf.Abs(speed.z);
+		moved.z += Mathf.Abs(speed.z);*/
+		Vector3 m_pos = transform.localPosition;
+		m_pos.z += 0.05f;
+		transform.localPosition = m_pos;  // 移動を更新
 
-		//折り返すか？
-		if (moved.x >= distance.x && moved.y >= distance.y && moved.z >= distance.z && turn) {
-			speed *= -1; //逆方向へ動かす
-			moved = Vector3.zero;
-		}
+
 	}
 }
