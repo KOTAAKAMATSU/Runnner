b07d6983c452effce461e97e417a50616f4a3b2d
KOTAAKAMATSU <redjuly2015@gmail.com>
2015-06-09 20:57:44 +0900

    ランニング演出改変中（床移動からキャラ移動へ）、床生成改変中（時間経過から接触判定へ）

---
+++ Assets/DestroyScriptFloor.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyScriptFloor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Destroy(gameObject, 5); //5秒後にGameObjectを削除
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
